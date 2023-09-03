2672ed9e60f14bbc852f835e842d22c2da2493f9
Paula Suarez <pauladzuck@hotmail.com>
2023-09-03 08:42:36 -0500

    Adding updates to product details.

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -6,7 +6,8 @@ import {
     Image,
     ListGroup,
     Card,
-    Button
+    Button,
+    ListGroupItem
 } from 'react-bootstrap';
 import Rating from '../components/Rating';
 
@@ -28,7 +29,6 @@ const ProductScreen = () => {
       </Col>
       <Col md={4}>
         <ListGroup variant="flush">
-
           <ListGroup.Item>
           <h3>{product.name}</h3>
           </ListGroup.Item>
@@ -38,10 +38,38 @@ const ProductScreen = () => {
           </ListGroup.Item>
 
           <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-        </ListGroup>
-        
+          <ListGroupItem>Description: {product.description}</ListGroupItem>
+        </ListGroup>    
       </Col>
       <Col md={3}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <Row>
+                <Col>Price:</Col>
+                <Col>
+                <strong>${product.price}</strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col>Status:</Col>
+                <Col>
+                <strong>{product.countInStock > 0 ? 'In Stock': 'Out of Stock'}</strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+              className='btn-block'
+              type='button'
+              disabled={product.countInStock === 0}
+              >Add to Cart
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
         
       </Col>
     </Row>
